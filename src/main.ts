b8d735111c152cd36f05b3da8d0669aa1df39574
Ben Chilcott <benchilcott@live.co.uk>
2021-12-24 04:26:53 +0000

    Added HTTPS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const fs = require('fs')
+
+const httpsOptions = {
+  key: fs.readFileSync('./secrets/benchilcott.com.key'),
+  cert: fs.readFileSync('./secrets/benchilcott.com.pem'),
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+	httpsOptions,
+  });
+  await app.listen(443);
 }
 bootstrap();
