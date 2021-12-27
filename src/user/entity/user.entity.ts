ac10760af8df57ff8821b42551de646df594a2d8
Ben Chilcott <38051283+bchilcott@users.noreply.github.com>
2021-12-27 04:09:25 +0000

    Add basic local/JWT authentication (#4)

---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,46 @@
+import {
+  BaseEntity,
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  BeforeInsert,
+} from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
+@Entity('user')
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid') id: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+    unique: true,
+  })
+  email: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+    select: false,
+  })
+  password: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+  })
+  name: string;
+
+  @Column()
+  @CreateDateColumn()
+  createdOn: Date;
+
+  @BeforeInsert() async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+
+  async validatePassword(password: string): Promise<boolean> {
+    return await bcrypt.compare(password, this.password);
+  }
+}
